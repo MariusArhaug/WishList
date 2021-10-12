9ef10db1f2f21a526d56b422562db4365028c635
MariusArhaug <mariusarhaug@hotmail.com>
2021-10-12 15:16:24 +0200

    DRAFT: fix merge errors

--- wishList/fxui/src/main/java/wishList/ui/AbstractController.java
+++ wishList/fxui/src/main/java/wishList/ui/AbstractController.java
@@ -1,6 +1,6 @@
 package wishList.ui;
 
-import wishList.core.User;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -8,19 +8,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import wishList.core.User;
 
 /** Abstract controller with changeScene method that all other controllers inherits. */
 public abstract class AbstractController implements ViewChanger {
 
-  private User user;
+  protected User user;
 
   void updateUser(User user) {
     this.user = user;
   }
 
-  protected void changeScene(String fileName, ActionEvent event, User user) throws IOException  {
+  protected void changeScene(String fileName, ActionEvent event, User user) throws IOException {
     FXMLLoader loader = new FXMLLoader();
-    loader.setLocation(getClass().getResource(fileName));
+    loader.setLocation(AbstractController.class.getResource(fileName));
     Parent newParent = loader.load();
 
     AbstractController newController = loader.getController();
