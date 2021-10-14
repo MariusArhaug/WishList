70c987cd91efbb61cb563bd0d99a72e82a07a9e5
Synne Ødegaard <synneo@stud.ntnu.no>
2021-10-14 08:37:04 +0200

    CHANGE: add tests

--- wishList/fxui/src/test/java/wishList/ui/MainViewControllerTest.java
+++ wishList/fxui/src/test/java/wishList/ui/MainViewControllerTest.java
@@ -1,4 +1,32 @@
 package wishList.ui;
 
-public class MainViewControllerTest {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(ApplicationExtension.class)
+public class MainViewControllerTest extends ApplicationTest {
+    private MainViewController controller;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("MainViewTest.fxml"));
+        final Parent root = loader.load();
+        this.controller = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @Test
+    public void testController(){
+        assertNotNull(this.controller);
+    }
+
 }
--- wishList/fxui/src/test/java/wishList/ui/ShowListViewControllerTest.java
+++ wishList/fxui/src/test/java/wishList/ui/ShowListViewControllerTest.java
@@ -1,28 +1,33 @@
 package wishList.ui;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.function.Predicate;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.util.WaitForAsyncUtils;
+import wishList.core.User;
 
-public class ShowListViewControllerTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(ApplicationExtension.class)
+public class ShowListViewControllerTest extends ApplicationTest {
+    private ShowListViewController controller;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("ShowListViewTest.fxml"));
+        final Parent root = loader.load();
+        this.controller = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @Test
+    public void testController(){
+        assertNotNull(this.controller);
+    }
+
 }
