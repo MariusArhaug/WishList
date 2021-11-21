897d81a293ceb5b543ba92c97cb158a15e764461
MariusArhaug <mariusarhaug@hotmail.com>
2021-11-21 16:01:21 +0100

    NO-ISSUE: fix test fxml id and tags

--- wishList/fxui/src/test/java/wishList/ui/LoginViewControllerTest.java
+++ wishList/fxui/src/test/java/wishList/ui/LoginViewControllerTest.java
@@ -7,124 +7,110 @@ import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.control.LabeledMatchers;
-import org.w3c.dom.ls.LSOutput;
 import wishList.core.User;
-import wishList.json.JsonHandler;
-import wishList.utils.Utils;
-
-import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(ApplicationExtension.class)
-public class LoginViewControllerTest extends ApplicationTest{
-    private LoginViewController controller;
-    private User user;
+public class LoginViewControllerTest extends ApplicationTest {
+  private LoginViewController controller;
+  private User user;
 
-    //public final String path =
-            //Utils.updatePathForAnyOs(
-                    //new File("").getAbsolutePath(), "src", "test", "resources", "wishList", "ui");
+  /**
+   * @param stage stage
+   * @throws Exception if file is not found
+   *     <p>Load fxml file, get controller from file
+   */
+  @Override
+  public void start(final Stage stage) throws Exception {
+    final FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginViewTest.fxml"));
+    final Parent root = loader.load();
+    this.controller = loader.getController();
+    stage.setScene(new Scene(root));
+    stage.show();
+  }
 
-    //private JsonHandler jsonHandler = new JsonHandler(path);
+  /** Check if user is null */
+  @BeforeEach
+  public void setup() {
+    this.user = null;
+  }
 
-    /**
-     *
-     * @param stage stage
-     * @throws Exception if file is not found
-     *
-     * Load fxml file, get controller from file
-     */
-    @Override
-    public void start(final Stage stage) throws Exception {
-        final FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginViewTest.fxml"));
-        final Parent root = loader.load();
-        this.controller = loader.getController();
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
+  /** Test that attributes' states are correct */
+  @Test
+  public void testController() {
+    assertNotNull(this.controller);
+    assertNull(this.user);
+  }
 
-    /**
-     * Check if user is null
-     */
-    @BeforeEach
-    public void setup(){
-        this.user = null;
-    }
+  /**
+   * Check that the label that displays error message works as planned
+   *
+   * @param robot executes operations in the GUI
+   */
+  @Test
+  public void verifyThatLabelChanges(FxRobot robot) {
+    FxAssert.verifyThat("#errorMessage", LabeledMatchers.hasText("Label"));
+    assertEquals(controller.errorMessage.getText(), "Label");
 
-    /**
-     * Test that attributes' states are correct
-     */
-    @Test
-    public void testController(){
-        assertNotNull(this.controller);
-        assertNull(this.user);
-    }
+    robot.clickOn("#login");
 
-    /**
-     * Check that the label that displays error message works as planned
-     * @param robot executes operations in the GUI
-     */
-    @Test
-    public void verifyThatLabelChanges(FxRobot robot){
-        FxAssert.verifyThat("#errorMessage", LabeledMatchers.hasText("Label"));
-        assertEquals(controller.errorMessage.getText(), "Label");
+    FxAssert.verifyThat(
+        "#errorMessage", LabeledMatchers.hasText("E-mail or password is incorrect"));
+    FxAssert.verifyThat(
+        controller.errorMessage, LabeledMatchers.hasText("E-mail or password is incorrect"));
+    assertEquals(controller.errorMessage.getText(), "E-mail or password is incorrect");
+  }
 
-        robot.clickOn("#login");
+  /**
+   * Test if existing user can be found in json file The user was added to file before the test
+   *
+   * @param robot executes the operations in the GUI
+   * @throws Exception
+   */
+  @Test
+  public void verifyThatUserExists(FxRobot robot) throws Exception {
+    robot.clickOn("#loginEmailInput");
+    robot.write("jane@doe.com");
+    robot.clickOn("#loginPasswordInput");
+    robot.write("qwerty123");
+    robot.clickOn("#login");
 
-        FxAssert.verifyThat("#errorMessage", LabeledMatchers.hasText("E-mail or password is incorrect"));
-        FxAssert.verifyThat(controller.errorMessage, LabeledMatchers.hasText("E-mail or password is incorrect"));
-        assertEquals(controller.errorMessage.getText(), "E-mail or password is incorrect");
-    }
+    // If errorMessage is "Label" after clicking on login,
+    // the user was found in the json file
+    assertEquals(controller.errorMessage.getText(), "Label");
 
-    /**
-     * Test if existing user can be found in json file
-     * The user was added to file before the test
-     * @param robot executes the operations in the GUI
-     * @throws Exception
-     */
-    @Test
-    public void verifyThatUserExists(FxRobot robot) throws Exception {
-        robot.clickOn("#loginEmailInput");
-        robot.write("jane@doe.com");
-        robot.clickOn("#loginPasswordInput");
-        robot.write("qwerty123");
-        robot.clickOn("#login");
+    controller.checkUser();
+    this.user = controller.user;
+    assertNotNull(this.user);
+    assertEquals(controller.loginEmailInput.getText(), this.user.getEmail());
+    assertEquals(controller.loginPasswordInput.getText(), this.user.getPassword());
+  }
 
-        //If errorMessage is "Label" after clicking on login,
-        //the user was found in the json file
-        assertEquals(controller.errorMessage.getText(), "Label");
+  /**
+   * Test if text fields work correctly
+   *
+   * @param robot executes operations in the GUI
+   */
+  @Test
+  public void testTextFields(FxRobot robot) {
+    assertEquals(controller.loginEmailInput.getText(), "");
+    robot.clickOn("#loginEmailInput");
+    robot.write("Hello");
+    assertEquals(controller.loginEmailInput.getText(), "Hello");
+    robot.clickOn("#login");
+    assertEquals(controller.loginEmailInput.getText(), "Hello");
 
-        controller.checkUser();
-        this.user = controller.user;
-        assertNotNull(this.user);
-        assertEquals(controller.loginEmailInput.getText(), this.user.getEmail());
-        assertEquals(controller.loginPasswordInput.getText(), this.user.getPassword());
-
-    }
-
-    /**
-     * Test if text fields work correctly
-     * @param robot executes operations in the GUI
-     */
-    @Test
-    public void testTextFields(FxRobot robot){
-        assertEquals(controller.loginEmailInput.getText(), "");
-        robot.clickOn("#loginEmailInput");
-        robot.write("Hello");
-        assertEquals(controller.loginEmailInput.getText(), "Hello");
-        robot.clickOn("#login");
-        assertEquals(controller.loginEmailInput.getText(), "Hello");
-
-        assertEquals(controller.loginPasswordInput.getText(), "");
-        robot.clickOn("#loginPasswordInput");
-        robot.write("there");
-        assertEquals(controller.loginPasswordInput.getText(), "there");
-        robot.clickOn("#login");
-        assertEquals(controller.loginPasswordInput.getText(), "there");
-    }
+    assertEquals(controller.loginPasswordInput.getText(), "");
+    robot.clickOn("#loginPasswordInput");
+    robot.write("there");
+    assertEquals(controller.loginPasswordInput.getText(), "there");
+    robot.clickOn("#login");
+    assertEquals(controller.loginPasswordInput.getText(), "there");
+  }
 }
